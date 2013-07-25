827aa871a1664600da4819caf880e6d49810640e
Chauncey <pairingmachine@rallydev.com>
2013-07-25 15:16:12 -0600

    fixing attributes file

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,8 @@
-default[:chromedriver][:version] = 'linux64_26.0.1383.0'
-default[:chromedriver][:archive_name]  = "chromedriver_#{node[:chromedriver][:version]}.zip"
-default[:chromedriver][:source_url]  = "https://chromedriver.googlecode.com/files/#{node[:chromedriver][:archive_name]}"
 default[:chromedriver][:path] = '/usr/bin'
 default[:chromedriver][:owner] = 'root'
 default[:chromedriver][:group] = 'root'
 default[:chromedriver][:mode] = '755'
+default[:chromedriver][:version] = 'linux64_26.0.1383.0'
+default[:chromedriver][:archive_name]  = "chromedriver_#{node[:chromedriver][:version]}.zip"
+default[:chromedriver][:source_url]  = "https://chromedriver.googlecode.com/files/#{node[:chromedriver][:archive_name]}"
 default[:chromedriver][:full_path] = "#{node[:chromedriver][:path]}/#{node[:chromedriver][:archive_name]}"
